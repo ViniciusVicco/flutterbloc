dfd163c24e2447340535345c203cdc6b1120df5b
viniciusvicco <viniciusvicco@gmail.com>
2021-12-12 11:42:52 -0300

    commit inicial do projeto com tdd + bloc

---
+++ lib/app_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutterbloc/start_home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const StartHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        "rota": (context) => StartHomePage(
+              title: "",
+            )
+      },
+    );
+  }
+}
